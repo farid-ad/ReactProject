a26229346bfd7bcbc35a4415ddf49b58fe926749
farid-ad <faridaboudiab123@gmail.com>
2021-09-27 21:02:45 +0300

    Add books

--- src/index.js
+++ src/index.js
@@ -1,16 +1,39 @@
 import React from 'react'
 import reactDom from 'react-dom'
 
-function Greeting() {
+function BookList() {
     return (
-        <div>
+        <section>
             <Person />
             <Message />
-        </div>
+            <Book />
+        </section>
     )
 }
 const Person = () => <h1>Farid Abou Diab</h1>
 const Message = () => <p>This is my message</p>
+const Image = () => (
+    <img
+        src="https://images-na.ssl-images-amazon.com/images/I/81bsw6fnUiL._AC_UL200_SR200,200_.jpg"
+        alt=""
+    />
+)
+const Book = () => {
+    return (
+        <article>
+            <Image> </Image>
+            <Title />
+            <Author />
+        </article>
+    )
+}
+const Title = () => (
+    <h3>
+        Rich Dad Poor Dad: What the Rich Teach Their Kids About Money That the
+        Poor and Middle Class Do Not!
+    </h3>
+)
+const Author = () => <h4>Robert T. Kiyosaki</h4>
 // const Greeting = () => {
 //     return React.createElement(
 //         'div',
@@ -19,4 +42,4 @@ const Message = () => <p>This is my message</p>
 //     )
 // }
 
-reactDom.render(<Greeting />, document.getElementById('root'))
+reactDom.render(<BookList />, document.getElementById('root'))
