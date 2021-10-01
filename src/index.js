ce867c66f59b2cf7862e964d80f4d64aaa9a6c4b
farid-ad <faridaboudiab123@gmail.com>
2021-10-01 19:40:55 +0300

    Proper list and spread operatore

--- src/index.js
+++ src/index.js
@@ -2,81 +2,64 @@ import React from "react";
 import reactDom from "react-dom";
 import "./index.css";
 
-const firstBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/71Ptolb7qnL._AC_UL200_SR200,200_.jpg",
-  title: "Peril",
-  autthor: "Bob Woodward",
-};
-const secondBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/81MgsNre3IL._AC_UL200_SR200,200_.jpg",
-  title: "The Wish",
-  autthor: "Nicholas Sparkss",
-};
+const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/71Ptolb7qnL._AC_UL200_SR200,200_.jpg",
+    title: "Peril",
+    autthor: "Bob Woodward",
+  },
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81MgsNre3IL._AC_UL200_SR200,200_.jpg",
+    title: "The Wish",
+    autthor: "Nicholas Sparkss",
+  },
 
-const thirdBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/81bsw6fnUiL._AC_UL200_SR200,200_.jpg",
-  title:
-    "Rich Dad Poor Dad: What the Rich Teach Their Kids About Money That the Poor and Middle Class Do Not!",
-  autthor: "Robert T. Kiyosaki",
-};
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81bsw6fnUiL._AC_UL200_SR200,200_.jpg",
+    title:
+      "Rich Dad Poor Dad: What the Rich Teach Their Kids About Money That the Poor and Middle Class Do Not!",
+    autthor: "Robert T. Kiyosaki",
+  },
 
-const fourthook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/71QKQ9mwV7L._AC_UL200_SR200,200_.jpg",
-  title:
-    "The Subtle Art of Not Giving a Fuck: A Counterintuitive Approach to Living a Good Life",
-  autthor: "Mark Manson",
-};
+  {
+    id: 4,
+    img: "https://images-na.ssl-images-amazon.com/images/I/71QKQ9mwV7L._AC_UL200_SR200,200_.jpg",
+    title:
+      "The Subtle Art of Not Giving a Fuck: A Counterintuitive Approach to Living a Good Life",
+    autthor: "Mark Manson",
+  },
 
-const fifthBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/61kp7tknNIS._AC_UL200_SR200,200_.jpg",
-  title:
-    "The Secret of Life: Rosalind Franklin, James Watson, Francis Crick, and the Discovery of DNA's Double Helix",
-  autthor: "Howard Markel",
-};
-const sixthBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/81RuAzKrHkL._AC_UL200_SR200,200_.jpg",
-  title: "The 5 Love Languages: The Secret to Love that Lasts Paperback",
-  autthor: "Gary Chapman",
-};
+  {
+    id: 5,
+    img: "https://images-na.ssl-images-amazon.com/images/I/61kp7tknNIS._AC_UL200_SR200,200_.jpg",
+    title:
+      "The Secret of Life: Rosalind Franklin, James Watson, Francis Crick, and the Discovery of DNA's Double Helix",
+    autthor: "Howard Markel",
+  },
+  {
+    id: 6,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81RuAzKrHkL._AC_UL200_SR200,200_.jpg",
+    title: "The 5 Love Languages: The Secret to Love that Lasts Paperback",
+    autthor: "Gary Chapman",
+  },
+];
 
 function BookList() {
   return (
     <section className="BookList">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        autthor={firstBook.autthor}
-      />
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        autthor={secondBook.autthor}
-      />
-      <Book
-        img={thirdBook.img}
-        title={thirdBook.title}
-        autthor={thirdBook.autthor}
-      />
-      <Book
-        img={fourthook.img}
-        title={fourthook.title}
-        autthor={fourthook.autthor}
-      />
-      <Book
-        img={fifthBook.img}
-        title={fifthBook.title}
-        autthor={fifthBook.autthor}
-      />
-      <Book
-        img={sixthBook.img}
-        title={sixthBook.title}
-        autthor={sixthBook.autthor}
-      />
+      {books.map(book => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
     </section>
   );
 }
 
 const Book = ({ img, title, autthor }) => {
+  // const { img, title, autthor } = props;
+
   return (
     <article className="Book">
       <img src={img} alt="" />
