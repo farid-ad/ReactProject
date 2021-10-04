00dd85e9d72ad526195b3f177ae5dc911d03017b
farid-ad <faridaboudiab123@gmail.com>
2021-10-04 16:21:32 +0300

    add event onClick

--- src/index.js
+++ src/index.js
@@ -60,11 +60,18 @@ function BookList() {
 const Book = ({ img, title, autthor }) => {
   // const { img, title, autthor } = props;
 
+  const clickHandler = () => {
+    alert("You choose a amazing book!!!");
+  };
+
   return (
     <article className="Book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{autthor}</h4>
+      <button type="button" onClick={clickHandler}>
+        Buy Now
+      </button>
     </article>
   );
 };
